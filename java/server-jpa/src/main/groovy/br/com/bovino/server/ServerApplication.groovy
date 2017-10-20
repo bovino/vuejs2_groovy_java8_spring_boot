0c192c5599fbede183cd8fe1fd37c23d8b852e75
Marcelo Bezerra <bezerra.mj@gmail.com>
2017-10-20 04:53:54 -0200

    transaction config

--- java/server-jpa/src/main/groovy/br/com/bovino/server/ServerApplication.groovy
+++ java/server-jpa/src/main/groovy/br/com/bovino/server/ServerApplication.groovy
@@ -2,8 +2,10 @@ package br.com.bovino.server
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class ServerApplication {
 
 	static void main(String[] args) {
